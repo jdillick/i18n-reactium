37a5426c0a3462dc1ee37ac4dbdd179694fd5aa9
John A Dillick <john@dillick.us>
2019-02-20 13:15:53 -0500

    Handle route update better.

--- src/app/components/i18n/reducers.js
+++ src/app/components/i18n/reducers.js
@@ -1,16 +1,19 @@
 import deps from 'dependencies';
+import op from 'object-path';
 
 export default (state = 'es_MX', action) => {
     switch (action.type) {
         case deps.actionTypes.SWITCH_LOCALE: {
             return state === 'en_US' ? 'es_MX' : 'en_US';
         }
-        case deps.actionTypes.UPDATE_ROUTE:
-            if (action.params.locale) {
-                action.locale = action.params.locale;
-            }
-        case deps.actionTypes.SET_LOCALE:
+
+        case deps.actionTypes.UPDATE_ROUTE: {
+            return op.get(action, 'params.locale', state);
+        }
+
+        case deps.actionTypes.SET_LOCALE: {
             return action.locale;
+        }
     }
     return state;
 };
