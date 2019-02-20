35e16946f1a570b99469a0305dfc6b7eee7743b1
John A Dillick <john@dillick.us>
2019-02-20 13:05:49 -0500

    Add routing.

--- src/app/components/InternationalizeTest/index.js
+++ src/app/components/InternationalizeTest/index.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 import { I18nProvider, Translate } from 'components/i18n';
 import SomeTranslatedComponent from './SomeTranslatedComponent';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import op from 'object-path';
 
 /**
  * -----------------------------------------------------------------------------
  * React Component: InternationalizeTest
  * -----------------------------------------------------------------------------
  */
-// const __ = message => message; // fake gettext
 
-export default class InternationalizeTest extends Component {
+class InternationalizeTest extends Component {
     render() {
         return (
-            <I18nProvider>
-                <Translate>
-                    <SomeTranslatedComponent />
-                </Translate>
-            </I18nProvider>
+            <div>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        padding: '10px',
+                        width: '300px',
+                    }}>
+                    <Link to={'/en_US'}>US English</Link>
+                    <Link to={'/es_MX'}>Español de Mexico</Link>
+                </div>
+
+                <I18nProvider>
+                    <Translate>
+                        <SomeTranslatedComponent />
+                    </Translate>
+                </I18nProvider>
+            </div>
         );
     }
 }
+
+export default withRouter(InternationalizeTest);
--- src/app/components/i18n/reducers.js
+++ src/app/components/i18n/reducers.js
@@ -1,11 +1,15 @@
-import actionTypes from './actionTypes';
+import deps from 'dependencies';
 
 export default (state = 'es_MX', action) => {
     switch (action.type) {
-        case actionTypes.SWITCH_LOCALE: {
+        case deps.actionTypes.SWITCH_LOCALE: {
             return state === 'en_US' ? 'es_MX' : 'en_US';
         }
-        case actionTypes.SET_LOCALE:
+        case deps.actionTypes.UPDATE_ROUTE:
+            if (action.params.locale) {
+                action.locale = action.params.locale;
+            }
+        case deps.actionTypes.SET_LOCALE:
             return action.locale;
     }
     return state;
